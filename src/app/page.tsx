695d7ebc4cf01c940c72d8a6f69c6a591871ff26
kraken <solodarken@gmail.com>
2024-06-17 13:44:57 +0530

    some other changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 import DotPattern from "@/components/DotPattern";
-import GridPattern from "@/components/GridPattern";
 import Image from "next/image";
+import profilePic from "../../public/profile-pic.png";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-screen w-screen flex flex-col gap-10 items-center justify-center">
       <DotPattern
         className="[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]"
         height={30}
         width={30}
       />
-      <div className="text-3xl font-bold">
+      <Image
+        src={profilePic}
+        alt="Ah, it didnt load :("
+        width={"280"}
+        height={"280"}
+      ></Image>
+
+      <div className="text-6xl leading-[60px] font-bold">
         <strong>Hey, 👋🏻</strong>
         <br /> I am kraken
       </div>
