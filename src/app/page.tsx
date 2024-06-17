fd6de11c6e1aac9285a5b49afea3970f925679ee
kraken <solodarken@gmail.com>
2024-06-18 00:26:05 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,55 @@
-import DotPattern from "@/components/DotPattern";
 import Image from "next/image";
 import profilePic from "../../public/profile-pic.png";
+import { OnlineTag } from "@/components/onlineTag";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col gap-10 items-center justify-center">
-      <DotPattern
-        className="[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]"
-        height={30}
-        width={30}
-      />
-      <Image
-        src={profilePic}
-        alt="Ah, it didnt load :("
-        width={"280"}
-        height={"280"}
-      ></Image>
+    <div className=" dark:bg-grid-white/[0.2] bg-grid-white/[0.2] h-screen w-screen flex flex-col md:flex-row ">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-      <div className="text-6xl leading-[60px] font-bold">
-        <strong>Hey, 👋🏻</strong>
-        <br /> I am kraken
+      <div className="w-1/2 px-1/2 md:w-full flex flex-col gap-10 items-center justify-center">
+        <div className="   relative flex items-center justify-center"></div>{" "}
+        <Image
+          src={profilePic}
+          alt="Ah, it didnt load :("
+          width={"320"}
+          height={"320"}
+        ></Image>
+        <div>
+          <OnlineTag />
+          <div className=" flex w-[320px] flex-col gap-4">
+            <div className="text-6xl leading-[60px] font-bold z-10">
+              <strong>Hey, 👋🏻</strong>
+              <br /> I am <strong className="text-[#f0ed78]">kraken</strong>
+            </div>
+            <div className="m-0 text-lg">
+              Full Stack Developer crafting sleek and modern web applications.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2  md:w-full z-10 h-screen flex flex-col gap-8 items-center justify-center">
+        <div className="w-2/3 flex flex-col gap-4 border-[1px] border-gray-500 p-3 rounded-lg">
+          <h1 className="text-2xl font-bold flex items-center gap-2">
+            <ArrowRightIcon className="font-bold" color="#f0ed78" /> Freelance
+            Experience
+          </h1>
+          <p>
+            I have freelanced as a Node.js developer (discord.js) on Fiverr,
+            earning over 100 five-star reviews for delivering high-quality,
+            reliable, and innovative solutions.
+          </p>
+        </div>
+        <div className="w-2/3 flex flex-col gap-4 border-[1px] border-gray-500 p-3 rounded-lg">
+          <h1 className="text-2xl font-bold flex items-center gap-2">
+            <ArrowRightIcon className="font-bold" color="#f0ed78" /> InvougeChat{" "}
+          </h1>
+          <p>
+            Developed a chatbot with AI, implementing RAG Arch. and answering
+            question blazingly fast
+          </p>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/onlineTag.tsx
@@ -0,0 +1,7 @@
+export const OnlineTag = () => {
+  return (
+    <div className="border-[1px] text-center z-100 w-fit px-7 py-1 rounded-full cursor-pointer">
+      🟢 Online
+    </div>
+  );
+};
