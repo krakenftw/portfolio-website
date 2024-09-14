f36ad6b67faf254c07af03c80c55ff3b2fe3be90
kraken <solodarken@gmail.com>
2024-09-14 20:03:37 +0530

    google analy.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       >
         <body className={inter.className}>{children}</body>
       </ThemeProvider>
+      <GoogleTagManager gtmId="G-DDFQ28XEWE" />
     </html>
   );
 }
